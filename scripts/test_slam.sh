9e7222d7217b966479656c8ae9e4e11eb989b213
Dong-Won Shin <dongwonshin@gist.ac.kr>
2019-05-28 00:06:41 +0900

    map change

--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -2,7 +2,7 @@
 
 xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
-xterm -e " rosrun gmapping slam_gmapping" &
+xterm -e " rosrun gmapping slam_gmapping _linearUpdate:=0.1 _angularUpdate:=0.1 _particles:=50 _xmin:=-10 _xmax:=10 _ymin:=-10 _ymax:=10" &
 sleep 5
 xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
